291e425955581d6b45806297d2c2efa76fe15f60
melnichenkosa <melnichenkosa@402D-12>
2023-05-18 14:35:29 +0700

    Добавьте файлы проекта.

---
+++ DerevoProMaxSuper/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace DerevoProMaxSuper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tree<int> tree = new Tree<int>();
+            tree.Add(5);
+            tree.Add(3);
+            tree.Add(7);
+            tree.Add(2);
+            tree.Add(4);
+            tree.Add(6);
+
+            Console.WriteLine("Breadth-First Traversal:");
+            tree.BreadthFirstTraversal();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ DerevoProMaxSuper/Tree.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace DerevoProMaxSuper
+{
+
+    public class Tree<T> : IEnumerable<T> where T : IComparable<T>
+    {
+        private Node<T> root;
+
+        public void Add(T data)
+        {
+            if (root == null)
+            {
+                root = new Node<T>(data);
+            }
+            else
+            {
+                AddRecursive(root, data);
+            }
+        }
+
+        public void AddRecursive(Node<T> node, T data)
+        {
+            if (data.CompareTo(node.Data) < 0)
+            {
+                if (node.Left == null)
+                {
+                    node.Left = new Node<T>(data);
+                }
+                else
+                {
+                    AddRecursive(node.Left, data);
+                }
+            }
+            else
+            {
+                if (node.Right == null)
+                {
+                    node.Right = new Node<T>(data);
+                }
+                else
+                {
+                    AddRecursive(node.Right, data);
+                }
+            }
+        }
+
+        public Node<T> Search(T data)
+        {
+            return SearchRecursive(root, data);
+        }
+
+        public Node<T> SearchRecursive(Node<T> node, T data)
+        {
+            if (node == null || data.CompareTo(node.Data) == 0)
+            {
+                return node;
+            }
+
+            if (data.CompareTo(node.Data) < 0)
+            {
+                return SearchRecursive(node.Left, data);
+            }
+            else
+            {
+                return SearchRecursive(node.Right, data);
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return BreadthFirstTraversal().GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+         IEnumerable<T> BreadthFirstTraversal()
+        {
+            if (root == null)
+                yield break;
+
+            Queue<Node<T>> queue = new Queue<Node<T>>();
+            queue.Enqueue(root);
+
+            while (queue.Count > 0)
+            {
+                Node<T> current = queue.Dequeue();
+                yield return current.Data;
+
+                if (current.Left != null)
+                    queue.Enqueue(current.Left);
+
+                if (current.Right != null)
+                    queue.Enqueue(current.Right);
+            }
+        }
+    }
+}
